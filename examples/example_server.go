fca258e346e19037162fde7d6b85a53729ee2108
Sebastian Leicht <sl@fino.digital>
2020-03-22 20:07:22 +0100

    Use mongodb interface in example server impl

--- examples/example_server.go
+++ examples/example_server.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"context"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
+	"time"
 
+	"github.com/baba2k/graphql-rungen/storage/mongodb"
 	"github.com/graphql-go/graphql"
 	"github.com/graphql-go/handler"
 	"github.com/vektah/gqlparser"
 	"github.com/vektah/gqlparser/ast"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
@@ -40,14 +45,28 @@ func main() {
 	})
 	schemaConfig := graphql.SchemaConfig{}
 
+	opt := options.Client()
+	opt = opt.SetHosts([]string{"localhost"})
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	db, err := mongodb.NewMongoDB(ctx, opt, "graphql")
+	if err != nil {
+		panic("can not connect to database: " + err.Error())
+	}
+
 	q := (*schema).Query
 	if q != nil {
 		fields := graphql.Fields{}
 		for _, field := range q.Fields {
-			fields[field.Name] = &graphql.Field{
+			if strings.HasPrefix(field.Name, "_") {
+				continue
+			}
+			name := field.Name
+			log.Println("Found field: ", name)
+			fields[name] = &graphql.Field{
 				Type: graphql.String,
 				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-					return "world", nil
+					ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+					return db.ReadAll(ctx, name)
 				},
 			}
 		}
@@ -66,10 +85,14 @@ func main() {
 
 	// TODO: Start graphql server
 	h := handler.New(&handler.Config{
-		Schema:   &s,
-		Pretty:   true,
-		GraphiQL: false,
+		Schema:     &s,
+		Pretty:     true,
+		GraphiQL:   false,
+		Playground: true,
 	})
 	http.Handle("/graphql", h)
-	http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic("graphql server can not be started: " + err.Error())
+	}
 }
