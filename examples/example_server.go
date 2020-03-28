e8ca7bb474272f10d0438497ce15135b6265b3d6
Sebastian Leicht <sl@fino.digital>
2020-03-28 23:58:24 +0100

    Add first simple mutation impl

--- examples/example_server.go
+++ examples/example_server.go
@@ -96,6 +96,58 @@ func main() {
 		schemaConfig.Query = graphql.NewObject(rootQuery)
 	}
 
+	// mutation
+	m := (*schema).Mutation
+	if m != nil {
+		fields := graphql.Fields{}
+		for _, field := range m.Fields {
+			if strings.HasPrefix(field.Name, "_") {
+				continue
+			}
+			name := field.Name
+			log.Println("Found mutation field: " + name)
+
+			// set field type
+			fields[name] = &graphql.Field{
+				Type: scalarTypesMap[field.Type.String()],
+			}
+
+			// set field arguments
+			fields[name].Args = make(graphql.FieldConfigArgument)
+			for _, arg := range field.Arguments {
+				fields[name].Args[arg.Name] = &graphql.ArgumentConfig{
+					Type:         scalarTypesMap[arg.Type.String()],
+					DefaultValue: arg.DefaultValue,
+					Description:  arg.Description,
+				}
+			}
+
+			// set field resolver
+			if strings.HasPrefix(name, "create") {
+				fields[name].Resolve = func(p graphql.ResolveParams) (interface{}, error) {
+					ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+					return db.Create(ctx, strings.ToLower(strings.TrimLeft(name, "create")), p.Args)
+				}
+			} else if strings.HasPrefix(name, "update") {
+				fields[name].Resolve = func(p graphql.ResolveParams) (interface{}, error) {
+					ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+					err := db.Update(ctx, strings.ToLower(strings.TrimLeft(name, "update")), p.Args, p.Args["id"])
+					return nil, err
+				}
+			} else if strings.HasPrefix(name, "delete") {
+				fields[name].Resolve = func(p graphql.ResolveParams) (interface{}, error) {
+					ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+					err := db.Delete(ctx, strings.ToLower(strings.TrimLeft(name, "delete")), p.Args["id"])
+					return nil, err
+				}
+			} else {
+				log.Println("unknown field " + name)
+			}
+		}
+		rootMutation := graphql.ObjectConfig{Name: m.Name, Fields: fields}
+		schemaConfig.Mutation = graphql.NewObject(rootMutation)
+	}
+
 	s, err := graphql.NewSchema(schemaConfig)
 	if err != nil {
 		log.Fatalf("failed to create new schema, error: %v", err)
