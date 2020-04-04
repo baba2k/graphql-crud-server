fd91f8ca1cc83236469bc41d469e893f901d9209
Sebastian Leicht <sl@fino.digital>
2020-04-05 00:13:49 +0200

    Add mapdb todo example

--- example/mapdb/example_mapdb_server.go
+++ example/mapdb/example_mapdb_server.go
@@ -1,27 +1,21 @@
 package main
 
 import (
-	"context"
 	"log"
-	"time"
 
 	"github.com/baba2k/graphql-crud-server"
 	"github.com/baba2k/graphql-crud-server/storage"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
 	// load schema from file
-	schema, err := graphql.LoadSchemaFromFile("example/schema/todo.graphql")
+	schema, err := graphql.LoadSchemaFromFile("example/mapdb/schema/todo.graphql")
 	if err != nil {
 		log.Fatal("can load schema: " + err.Error())
 	}
 
-	// create mongodb database interface
-	opt := options.Client()
-	opt = opt.SetHosts([]string{"localhost"})
-	ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
-	db, err := storage.NewMongoDB(ctx, opt, "graphql")
+	// create map database interface
+	db, err := storage.NewMapDB()
 	if err != nil {
 		log.Fatal("can not connect to database: " + err.Error())
 	}
